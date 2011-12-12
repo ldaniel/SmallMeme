ffd533d6bdea9388ee2f420232de56467dd34197
ldaniel <contato@leandrodaniel.com>
2011-12-11 22:53:34 -0200

    Refactoring

---
+++ src/SmallMeme/Meme.cs
@@ -0,0 +1,85 @@
+﻿using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+
+public class Meme
+{
+    public string BasePath { get; private set; }
+    public string FileExtension { get; private set; }
+    public string OutputName { get; private set; }
+    public string BaseMeme { get; private set; }
+    public string TopText { get; private set; }
+    public string BottomText { get; private set; }
+    public string Status { get; private set; }
+
+    public Meme()
+    {
+        BasePath = Directory.GetCurrentDirectory() + @"\memes\";
+        FileExtension = ".jpg";
+        OutputName = "meme";
+        BaseMeme = "yuno";
+        TopText = "HEY!";
+        BottomText = "Y U NO TYPE \"HELP\"?";
+        Status = string.Empty;
+    }
+
+    public Bitmap GetMemeBaseImage()
+    {
+        return new Bitmap(BasePath + BaseMeme + FileExtension);
+    }
+
+    public void Generate(string[] args)
+    {
+        CheckParameters(args);
+        CreateMemeImage();
+        UpdateStatus("small meme created -> " + BasePath + BaseMeme + FileExtension);
+    }
+
+
+    public string GetStatus()
+    {
+        return Status;
+    }
+
+    private void CheckParameters(string[] args)
+    {
+        if (MemeValidator.CheckParameters(args))
+        {
+            BaseMeme = args[0];
+            TopText = args[1];
+            BottomText = args[2];
+        }
+    }
+
+    private void CreateMemeImage()
+    {
+        var memeBase = GetMemeBaseImage();
+
+        using (var bitmap = new Bitmap(memeBase.Width, memeBase.Height))
+        {
+            using (var graphics = Graphics.FromImage(bitmap))
+            {
+                graphics.DrawImage(memeBase, 0, 0, memeBase.Width, memeBase.Height);
+                DrawText(graphics);
+            }
+            bitmap.Save(BasePath + OutputName + FileExtension, ImageFormat.Jpeg);
+        }
+    }
+
+    private void DrawText(Graphics graphics)
+    {
+        graphics.DrawString(TopText, GetFont(), Brushes.White, new Point(100, 10));
+        graphics.DrawString(BottomText, GetFont(), Brushes.White, new Point(100, 250));
+    }
+
+    private void UpdateStatus(string text)
+    {
+        Status = text;
+    }
+
+    private Font GetFont()
+    {
+        return new Font("Verdana", 18, FontStyle.Bold);
+    }
+}
---
+++ src/SmallMeme/MemeValidator.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+public static class MemeValidator
+{
+    public static bool CheckParameters(string[] args)
+    {
+        bool result = false;
+
+        if (args.Count() == 3)
+        {
+            bool isFirstParameterValid = CheckFirstParameter(args[0]);
+            bool isSecondParameterValid = CheckSecondParameter(args[1]);
+            bool isThirdParameterValid = CheckThirdParameter(args[2]);
+
+            result = isFirstParameterValid && isSecondParameterValid && isThirdParameterValid;
+        }
+
+        return result;
+    }
+
+    private static bool CheckFirstParameter(string firstParameter)
+    {
+        return CheckMemeType(firstParameter);
+    }
+
+    private static bool CheckSecondParameter(string secondParameter)
+    {
+        return secondParameter.Length > 0;
+    }
+
+    private static bool CheckThirdParameter(string thirdParameter)
+    {
+        return thirdParameter.Length > 0;
+    }
+    
+    private static bool CheckMemeType(string meme)
+    {
+        return GetMemesFromMemeDirectory().Contains(meme);
+    }
+
+    private static List<string> GetMemesFromMemeDirectory()
+    {
+        var files = Directory.EnumerateFiles(Directory.GetCurrentDirectory() + @"\memes\");
+        var memePossibleValues = new List<string>();
+        foreach (var file in files)
+        {
+            memePossibleValues.Add(Path.GetFileName(file).Replace(".jpg", ""));
+        }
+
+        return memePossibleValues;
+    }
+}
